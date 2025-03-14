997c46643df3cf2ceaa5b2872142f7ef8b4c188c
kratika <kratikadeshlehra@gmail.com>
2025-03-14 19:14:42 +0530

    created login page

--- chat-app/src/components/Login.jsx
+++ chat-app/src/components/Login.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import {FaSignInAlt} from 'react-icons/fa'
 const Login = () => {
   return (
-    <div>
+        <section className='h-[100vh] flex flex-col justify-center items-center background-image'>
+                <div className='bg-white shadow-lg p-5 rounded-xl h-[27rem] w-[20rem] flex flex-col justify-center items-center'>
+                  <div className='mb-10'>
+                      <h1 className='text-center text-[28px] font-bold'>Sign In</h1>
+                      <p className='text-center text-sm text-gray-400'>Welcome back, Login In to continue</p>
+                  </div>
+                  <div className="w-full">
+                    <input type='email' className='border border-green-500 w-full p-2 rounded-md bg-[#01aa851d] text-[#004939f3] mb-3 font-medium outline-none placeholder:text-[#00493958]' placeholder='Email'/>
+                    <input type='password' className='border border-green-500 w-full p-2 rounded-md bg-[#01aa851d] text-[#004939f3] mb-3 font-medium outline-none placeholder:text-[#00493958]' placeholder='Password'/>   
+                  </div> 
       
-    </div>
+                  <div className='w-full'>
+                    <button className='bg-[#01aa85] text-white font-bold w-full p-2 rounded-md flex items-center gap-2 justify-center '>  Login In
+                      <FaSignInAlt/>
+                    </button>
+                  </div>
+                   
+                   <div className='mt-5 text-center text-gray-400 text-sm'>
+                    <button>Don't have an account yet? Sign up</button>
+                   </div>
+                </div>
+            </section>
   )
 }
 
